337fb22290b02ab71ae5dee3fe98748e05722ca6
Davi Oliveira <davioliveira.java@gmail.com>
2019-11-11 14:38:34 -0300

    Adicionando jquery, lib chamada jquery.mask e alterações no cadastro!

--- validacao.js
+++ validacao.js
@@ -1,10 +1,6 @@
-function mostrarSenha(){
-    var tipo = document.getElementById("senha");
-    if(tipo.type == "password")
-    {
-        tipo.type = "text";
-    }
-    else{
-        tipo.type = "password";
-    }
-}
+$(function() {
+    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.rg').mask('0000000000-0');
+    $('.phone_with_ddd').mask('(00) 00000-0000');
+    $('.cep').mask('00000-000');
+});

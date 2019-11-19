437c20fabb8453ad9793d611b726de331a5bacce
daviziks-pc <davioliveira.java@gmail.com>
2019-11-19 02:58:26 -0300

    Alterado arquivos, cadastro(html/css), arquivos JS e adicionado framework materialize.css

--- validacao.js
+++ validacao.js
@@ -1,6 +1,8 @@
 $(function() {
-    $('.cpf').mask('000.000.000-00', {reverse: true});
-    $('.rg').mask('0000000000-0');
-    $('.phone_with_ddd').mask('(00) 00000-0000');
     $('.cep').mask('00000-000');
+    $('.phoneWithDDD').mask('(00) 00000-0000');
+    $('.dataNasc').mask('00/00/0000');
+    $('.rg').mask('0000000000-0');
+    $('.cpf').mask('000.000.000-00', {reverse: true});
+    
 });

daa4002cb6f67f4584c1b6225e28f8be55f4ad07
Davi Oliveira <davioliveira.java@gmail.com>
2019-11-21 06:45:17 -0300

    !Alterações na organização e estrutura do Sistema!

--- cadastrar.php
+++ cadastrar.php
@@ -1,7 +1,6 @@
 <?php
 
 include_once("conexao.php");
-
 $nome = $_POST['nome'];
 $endereco = $_POST['endereco'];
 $cep = $_POST['cep'];
@@ -12,10 +11,18 @@ $rg = $_POST['rg'];
 $sexo = $_POST['sexo'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+$imagem = $_FILES['imagem'];
+$pesquisa = mysqli_query($conexao,"SELECT * FROM `tb_usuario` 
+WHERE `email` = '$email' AND `cpf`= '$cpf'");
 
 if(empty($nome) || empty($endereco) || empty($cep)|| empty($fone)|| empty($data_nasc)|| empty($rg)|| empty($cpf)|| empty($sexo)|| empty($email)|| empty($senha)){
     echo("<script type='text/javascript'>
     alert(Preencha todos os campos!');
+    </script>");
+}
+else if(mysqli_num_rows($pesquisa) > 0){
+    echo("<script type='text/javascript'>
+    alert('Há um cadastro igual ao seu!');
     </script>");
 }
 else{
@@ -32,4 +39,27 @@ if(mysqli_insert_id($conexao))
 else{
     die("Falha na conexão: ". mysqli_connect_error());
 }
+
 ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <title>Salão de Cabelereiro</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card-panel panel">
+                <h1>Seja Bem-Vindo(a)!</h1>
+                <a href="index.html"><center><input type="button" value="Fazer login" class="submit"></center></a>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ logar.php
@@ -0,0 +1,29 @@
+<?php
+
+
+include_once("conexao.php");
+session_start();
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+
+$result = mysqli_query($conexao,"SELECT * FROM `tb_usuario` 
+WHERE `email` = '$email' AND `senha`= '$senha'");
+
+if(mysqli_num_rows ($result) > 0 )
+{
+$_SESSION['email'] = $email;
+$_SESSION['senha'] = $senha;
+header('location:home.html');
+}
+else{
+  unset ($_SESSION['email']);
+  unset ($_SESSION['senha']);
+  header('location:index.html');
+  echo("<script type='text/javascript'>
+  alert('Você não possui cadastro!');
+</script>");
+  }
+
+?>

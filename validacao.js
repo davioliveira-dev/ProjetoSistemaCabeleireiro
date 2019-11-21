f565d9e05e7706443af78b749eb4e5101acbe0f6
daviziks-pc <davioliveira.java@gmail.com>
2019-11-21 06:48:17 -0300

    Removido arquivos que estão em suas respectivas pastas

--- validacao.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.cep').mask('00000-000');
-    $('.phoneWithDDD').mask('(00) 00000-0000');
-    $('.dataNasc').mask('00/00/0000');
-    $('.rg').mask('0000000000-0');
-    $('.cpf').mask('000.000.000-00', {reverse: true});
-    
-});

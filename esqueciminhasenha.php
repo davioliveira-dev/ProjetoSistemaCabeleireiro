20019dfb407b84c798d20f3bdc1bfd9203c34e48
Davi Oliveira <davioliveira.java@gmail.com>
2019-11-23 01:03:52 -0300

    Atualizações nas validações dos formulários!

---
+++ esqueciminhasenha.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once("conexao.php");
+
+$email = $_POST['email'];
+
+$result = mysqli_query($conexao,"SELECT * FROM `tb_usuario` 
+WHERE `email` = '$email' ");
+
+if(mysqli_num_rows ($result) > 0 )
+{
+    echo("<script type='text/javascript'>
+    alert('Um e-mail foi enviado para recuperação da sua senha!');
+  </script>");
+  echo("<center><a href='index.html'>Voltar ao Login</a></center>");
+}
+else{
+    echo("<script type='text/javascript'>
+    alert('Você não possui um cadastro com esse email!');
+  </script>");
+  echo("<center><a href='esqueciminhasenha.html'>Voltar ao início</a></center>");
+}
+
+?>
--- js/validacao.js
+++ js/validacao.js
@@ -21,4 +21,66 @@ function validarSenha(){
 }
 senha.onchange = validarSenha;
 confSenha.onkeyup = validarSenha;
+botao = document.getElementById("botao");
+botao2 = document.getElementById("botaoConfSenha");
 
+function mostrarsenha(){
+    botao.addEventListener("mouseup",mouseUp);
+    botao.addEventListener("mousedown",mouseDown);
+    botao2.addEventListener("mouseup",mouseUp);
+    botao2.addEventListener("mousedown",mouseDown);
+}
+function mouseUp(){
+    senha.type = "password";
+    confSenha.type = "password";
+}
+function mouseDown(){
+    senha.type = "text";
+    confSenha.type = "text";
+}
+document.getElementById('sexo').addEventListener('keypress', function(evt) {
+    if (evt.key != 'f' && evt.key != 'm' && evt.key != 'F' && evt.key != 'M') {
+        evt.preventDefault()
+        alert('Tecla inválida, digite M ou F!');
+    }
+});
+
+function validarCPF(cpf) {	
+	cpf = cpf.replace(/[^\d]+/g,'');	
+	if(cpf == '') return alert('CPF Inválido!') + false + trocaClasse(cpf);;	
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 || 
+		cpf == "00000000000" || 
+		cpf == "11111111111" || 
+		cpf == "22222222222" || 
+		cpf == "33333333333" || 
+		cpf == "44444444444" || 
+		cpf == "55555555555" || 
+		cpf == "66666666666" || 
+		cpf == "77777777777" || 
+		cpf == "88888888888" || 
+		cpf == "99999999999")
+			return alert('CPF Inválido!') + false + trocaClasse(cpf);;		
+	// Valida 1o digito	
+	add = 0;	
+	for (i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+			return alert('CPF Inválido!') + false + trocaClasse(cpf);;		
+	// Valida 2o digito	
+	add = 0;	
+	for (i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+		return alert('CPF Inválido!') + false + trocaClasse(cpf);		
+    return true;
+}
+function trocaClasse(cpf) {
+    alert("Tá errado");
+}
